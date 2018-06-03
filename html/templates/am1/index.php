df3283165a293080b756d50d1ae21695e6c2096b
Logan <logna.shaner@gmail.com>
2018-06-03 02:45:31 -0400

    Continue install setup

--- html/templates/am1/index.php
+++ html/templates/am1/index.php
@@ -1,17 +1,39 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml"
    xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
    <head>
+     <!-- bootstrap3.3.7 -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+     <!-- bootstrap default theme -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+     <!-- custom -->
      <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/maintheme.css" type="text/css" />
-     <jdoc:include type="head" />
-     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
-     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
    </head>
-    <body>
-      <jdoc:include type="modules" name="top" />
-      <jdoc:include type="component" />
-      <jdoc:include type="modules" name="bottom" />
-    </body>
+   <body>
+     <div class="container">
+       <div id="nav" class="row">
+         <div id="logo" class="col-md-1">
+             <img src="/images/logo/logo-1-06.png"/>
+         </div>
+         <div class="col-md-11">
+           <div id="navbar">
+             <img class="social-img" src="/images/logo/social/ig-color.svg"/>
+             <img class="social-img" src="/images/logo/social/spot-color.svg"/>
+             <img class="social-img" src="/images/logo/social/sc-bnw.svg"/>
+             <a href="#">About</a>
+             <a href="#">Contact</a>
+             <a href="#">Social</a>
+           </div>
+         </div>
+       </div>
+     </div>
+     <div class="container">
+       <div id="jumbo" class="jumbotron">
+         <img src="/images/lg/IMG_0111.jpg"
+       </div>
+     </div>
+     Hello world
+   </body>
 </html>
