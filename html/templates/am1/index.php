fbb38f32c545b7ea2b5bedfbf28eefabd6d8ce85
Logan <logna.shaner@gmail.com>
2018-06-28 12:56:30 -0400

    updated mobile, refactor

--- html/templates/am1/index.php
+++ html/templates/am1/index.php
@@ -3,19 +3,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml"
    xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
    <head>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Google Font: PT Sans Narrow -->
-     <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
-     <!-- bootstrap3.3.7 -->
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-     <!-- bootstrap default theme -->
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- bootstrap js -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- custom -->
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/maintheme.css"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Google Font: PT Sans Narrow -->
+      <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet">
+      <!-- bootstrap3.3.7 -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <!-- bootstrap default theme -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+      <!-- bootstrap js -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <!-- custom -->
+      <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/maintheme.css"/>
    </head>
 
 
@@ -33,7 +33,7 @@
 
 
     <div id="main" class="container">
-
+      <!-- jumbo here -->
       <div class="jumbotron">
         <img id="jumboimg" src="<?php echo $this->beaseurl ?>/images/am-img/lg/full-bandstage.JPG">
       </div>
@@ -41,7 +41,6 @@
 
       <div id="bio-section" class="row">
         <div id="bio" class="col-md-6">
-          <!-- contact form here -->
           <h2> BIO </h2>
           <p>
             Hard Rock/Alternative Metal, based in Indianapolis, IN. Formed in 2018, memebers consist of Alexander Michael, Kerry Mills, Bram Epstein, and Zach Rayburn.
@@ -49,6 +48,7 @@
           <p id="booking"><strong>For booking: 317.724.0957</strong></p>
           <p id="booking"><strong>Email: imalexandermichael@gmail.com</strong></p>
           <br>
+          <!-- contact form here -->
           <div class="contact-form">
             <div class="form-group">
               <label for="name">Name * </label>
@@ -106,13 +106,14 @@
 
    <!--
    =============== TODO LIST ===============
-   Build out nav-bar, and hamburger
+   Build out nav-bar and hamburger
    Add news section (hook in joomla)
+   joomla user accounts
+   joomla news article mobile design
    work on responsiveness
    finish frame
    Font family setup  ***done***
-   Image selection (replace bio img?)
-   ssl cert
-   domain info
+   Image selection - replace bio-img? **Changed for mobile**
+   ssl cert **domain info**
    prep for stage
  -->
